7808943b5414005f83c565bc1b83034883bdeaaf
Dan{ } <123217422+DanSSV@users.noreply.github.com>
2023-03-13 16:33:59 +0800

    Add files via upload

--- lastupdate.php
+++ lastupdate.php
@@ -1,19 +1,24 @@
-$mysqli = new mysqli("localhost", "username", "password", "database_name");
+<?php
+$mysqli = new mysqli("localhost", "root", "", "sale");
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
     exit();
 }
 
-// Get the current date and time
 $current_time = time();
 
 $time_limit = strtotime('-6 months', $current_time);
 
-$sql = "UPDATE your_table SET status = 2 WHERE last_updated < '$time_limit'";
+$sql = "UPDATE product SET status = 2 WHERE last_updated < '$time_limit'";
 
 if (!$mysqli->query($sql)) {
     echo "Error updating data: " . $mysqli->error;
 }
 
 $mysqli->close();
+?>
+
+<!-- <form method="post" action="lastupdate.php">
+  <button type="submit" name="update_data" class="btn btn-primary">Update Data</button>
+</form> -->
